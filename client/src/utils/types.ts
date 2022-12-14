334341653e2ef0854d22e07ddf65ea470669e853
Maria Pallarés <maria.pallares@bitwala.com>
2022-12-14 21:22:25 +0100

    feat: add type User and type Project

---
+++ client/src/utils/types.ts
@@ -0,0 +1,19 @@
+export interface User {
+  createdAt: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  longDescription: string;
+  company: string;
+  address: string;
+  phone: string;
+  shortDescription: string;
+  projects?: [Project];
+  id: string;
+}
+
+export interface Project {
+  city: string;
+  description: string;
+  phone: string;
+}
